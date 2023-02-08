6df81b64ee0df892f80c3b81c96b42889709e0be
Rohan Kumar <119226198+Rohan043@users.noreply.github.com>
2023-02-08 20:25:06 +0530

    Create LinearSearch.py

---
+++ LinearSearch.py
@@ -0,0 +1,12 @@
+# Linear Search
+arr=[1,2,3,4,5,6]
+b=4 #b=int(3.5) or b=8 or b=3.5
+flag=0
+for i in range(len(arr)):
+    if arr[i]==b:
+        print("element found")
+        #print(i) *for index value
+        flag=1
+        break
+    if flag==0:
+        print("Not Found")
