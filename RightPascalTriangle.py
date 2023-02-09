ea80f09e42e83b31beab637bb71f3b643dc409ac
Rohan Kumar <119226198+Rohan043@users.noreply.github.com>
2023-02-09 09:34:03 +0530

    Create RightPascalTriangle.py

---
+++ RightPascalTriangle.py
@@ -0,0 +1,23 @@
+n=5
+for i in range(0,n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print( )
+for i in range(n,0,-1):
+    for j in range(i-1):
+         print("*",end=" ")
+    print( )
+    
+    
+#output:
+'''
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+'''
